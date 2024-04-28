efeaa3d6b239887a31295fe6efb100abfa8b1cf6
TF1405 <ferdiyanata@gmail.com>
2024-04-28 21:14:34 +0700

    [Feat] Copyright Section and add border

---
+++ src/assets/components/Footer/Copyright.jsx
@@ -0,0 +1,18 @@
+const Copyright = () => {
+    return (
+        <div className="mt-10 relative">
+            <div className="-ml-32 -mr-32">
+                <div className="ds-border-b ds-border-whisper ds-w-full undefined" />
+            </div>
+            <div className="w-full flex justify-center space-y-1 text-base text-suva-grey flex-col align-items text-center p-4">
+                <p>
+                    itemku v3.22.5 © 2014 - 2023 PT. Five Jack All Rights
+                    Reserved.
+                </p>
+                <p>All other trademarks belong to their respective owners.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Copyright;
--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
+import Copyright from "./Copyright";
 import FirstColumn from "./FirstColumn/FirstColumn";
 import SecondColumn from "./SecondColumn/SecondColumn";
 
 const Footer = () => {
     return (
-        <div className="flex w-full justify-between space-x-8 desktop-small:space-x-16">
-            <FirstColumn/>
-            <SecondColumn/>
+        <div className="w-full mt-20 space-y-10 font-itemkuV2">
+            <div className="-ml-32 -mr-32">
+                <div className="ds-border-b ds-border-whisper ds-w-full undefined" />
+            </div>
+            <div className="flex w-full justify-between space-x-8 desktop-small:space-x-16">
+                <FirstColumn />
+                <SecondColumn />
+            </div>
+            <Copyright />
         </div>
     );
 };
