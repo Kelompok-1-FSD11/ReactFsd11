58b592c624ff9d33fede4592ae18dd2c955d6101
TF1405 <ferdiyanata@gmail.com>
2024-05-04 15:21:58 +0700

    [Feat] Beli Cepat

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import HomePage from './Pages/home'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomePage from './pages/home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './Pages/layout';
+import Layout from './pages/layout';
 import ContainerDetail from './components/module/container/container-detail';
 import Container from './components/module/container';
+import App from './App';
 
 const router = createBrowserRouter([
-{
-  path :"/",
-  element: <div>
-              <Layout/>
-          </div>,
-},
-{
-  path :"/1",
-  element: <div>
-              <Container/>
-          </div>,
-},
-{
-  path :"/rif",
-  element: <div>
-              <HomePage/>
-              <p>Detail Product</p>
-          </div>,
-}
+	{
+		path: '/',
+		element: (
+			<div>
+				<Layout />
+			</div>
+		),
+	},
+	{
+		path: '/1',
+		element: (
+			<div>
+				<Container />
+			</div>
+		),
+	},
+	{
+		path: '/rif',
+		element: (
+			<div>
+				<HomePage />
+				<p>Detail Product</p>
+			</div>
+		),
+	},
+	{
+		path: '/app',
+		element: (
+			<div>
+				<App/>
+			</div>
+		),
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
