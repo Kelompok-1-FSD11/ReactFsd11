19b3aa0367bce07555189eb505300df664dd9448
arif0410 <arif.ibrahim0410@gmail.com>
2024-05-05 01:22:19 +0700

    give up

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,11 +1,14 @@
 import Footer from "../../components/module/footer";
 import Navbar from "../../components/module/navbar";
+import HomePage from "../home";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      
       {children}
+      <HomePage />
       <Footer />
     </>
   );
