97b96a16d5a400e6a22debfe18ada0c297840ac9
arif0410 <arif.ibrahim0410@gmail.com>
2024-05-05 08:46:15 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import HomePage from './Pages/home'
+import HomePage from './pages/home.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './Pages/layout';
-import ContainerDetail from './components/module/container/container-detail';
-import Container from './components/module/container';
 
 const router = createBrowserRouter([
 {
   path :"/",
   element: <div>
-              <Layout/>
+              <p>Welcome</p>
           </div>,
+},
+{
+
+  path :"/home",
+  element: <div><HomePage/></div>,
 },
 {
   path :"/detail-product",
   element: <div>
-              <Container/>
-          </div>,
-},
-{
-  path :"/rif",
-  element: <div>
-             / <HomePage/>
+
               <p>Detail Product</p>
           </div>,
 }
